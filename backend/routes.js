18e6c1153589eff535435bfffc6c86d6bd34c855
tlnguyen4 <tlnguyen@princeton.edu>
2017-08-14 16:05:43 -0700

    setting up backend for scavenger

---
+++ backend/routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const models = require('../models/models');
+const User = models.User;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+  res.send('Heroku running!');
+})
+
+app.post('/register', function(req, res) {
+  new User({
+    name: req.body.name,
+    username: req.body.username,
+    password: req.body.password
+  }).save(function(err) {
+    if (err) {
+      res.send({
+        register: false,
+        error: err
+      })
+    } else {
+      res.send({
+        register: true,
+      });
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Backend server for Scavenger Hunt running on port 3000");
+});
---
+++ models/models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    required: true,
+    type: String
+  },
+  username: {
+    required: true,
+    type: String
+  },
+  password: {
+    required: true,
+    type: String
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+  User: User,
+};
